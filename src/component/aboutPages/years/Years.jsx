d26abfa629d400280958026b649295899732ae74
Kgaugelo <milkeyroise@gmail.com>
2024-08-27 03:52:36 +0100

    done with years

---
+++ src/component/aboutPages/years/Years.jsx
@@ -0,0 +1,24 @@
+import './year.css'
+import pic33 from '../../../assets/years.jpeg'
+
+const Years = () => {
+  return (
+    <div className='years'>
+      <div className="years1">
+        <p className="fuguegyuy1">8 Years With You</p>
+        <p className="ibyfhb2">Build & Sale</p>
+        <p className="dhbdfbhj3">
+        Daniel Makapane Projects has been offering construction management, general contracting, electrical, and plumbing services to Gauteng and Limpopo citizens since 2016. We specialise in new commercial construction, commercial renovations, and custom house building. Our experience encompasses educational, recreational, office, retail, industrial, and residential sectors, among others.
+        </p>
+        <p className="dhbdfbhj3">
+        We endeavour to give professional results that exceed all expectations because of our dedication to creating relationships and upholding honourable principles. Each project is unique. Our goal is to accomplish each one to a high grade, at a reasonable price, and on schedule, leaving each client with a desirable and compelling outcome.
+        </p>
+      </div>
+      <div className="years2">
+        <img src={pic33} alt="pic" />
+      </div>
+    </div>
+  )
+}
+
+export default Years
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Head from '../component/header/Head'
 import Footer from '../component/footer/Footer'
 import Mainone from '../component/aboutPages/main/mainone'
+import Years from '../component/aboutPages/years/Years'
 
 const About = () => {
   return (
     <>
       <Head />
-      <Mainone /> 
+      <Mainone />
+      <Years />
       <Footer />
     </>
   )
