b3257f746aaa69770c26b88c65b23ba580508c21
Kgaugelo <milkeyroise@gmail.com>
2024-08-27 03:34:15 +0100

    done with main

---
+++ src/component/aboutPages/main/mainone.jsx
@@ -0,0 +1,25 @@
+import './main.css'
+import picsss from '../../../assets/mainone.jpeg'
+
+const Mainone = () => {
+  return (
+    <div className='mainone'>
+      <div className="mainO">
+        <p className="lkuugfhb">Building with Integrity</p>
+        <p className="vcmnfbhj">
+          We specialize in new commercial building,
+          commercial renovation, and custom home building.
+          Our experience includes educational, recreational, offices,
+          retail spaces, industrial facilities, homes, and many more.
+        </p>
+        <p className="dfiybids">Get Your Quote Now</p>
+        <p className="fbgjldfhibrebjh">012 023 4759</p>
+      </div>
+      <div className="mainoo">
+        <img src={picsss} alt="img" className='mainooooo' />
+      </div>
+    </div>
+  )
+}
+
+export default Mainone
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Head from '../component/header/Head'
 import Footer from '../component/footer/Footer'
+import Mainone from '../component/aboutPages/main/mainone'
 
 const About = () => {
   return (
     <>
       <Head />
+      <Mainone /> 
       <Footer />
     </>
   )
