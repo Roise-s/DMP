8fdf5a346ff24c815e0daf29799f490c1dc70bdc
Kgaugelo <milkeyroise@gmail.com>
2024-08-27 04:40:52 +0100

    done with choose

---
+++ src/component/aboutPages/choose/Choose.jsx
@@ -0,0 +1,35 @@
+import './choose.css'
+
+const Choose = () => {
+  return (
+    <div className='choose'>
+      <p className="wdshgdhgf11">Why Choose Us</p>
+      <p className="euireycvchv2">We work to an extremely high standard of customer satisfaction</p>
+      <div className="fDVF">
+        <div className="dnjkvhbbh45">
+            <img src="" alt="" className='dfnkjfbh5786'/>
+            <p className="dugydfb345">Full Service</p>
+            <p className="dkjbdfhb0984">
+            Not sure where to start? Let us help. We can handle all aspects of your from planning to completion at Daniel Makapane Projects we are dedicated to providing you with full satisfactory service.
+            </p>
+        </div>
+        
+        <div className="dnjkvhbbh45">
+            <img src="" alt="" className='dfnkjfbh5786'/>
+            <p className="dugydfb345">Deliver Value</p>
+            <p className="dkjbdfhb09841">
+            Value means many things to different people. We pledge to always provide optimal value for each and every client.
+            </p>
+        </div>
+        
+        <div className="dnjkvhbbh45">
+            <img src="" alt="" className='dfnkjfbh5786'/>
+            <p className="dugydfb345">Foundation</p>
+            <p className="dkjbdfhb0984">Founded on the principles of quality, integrity, and hard work, Daniel Makapane Projects takes great pride in its accomplishments and builds on them everyday.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Choose
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,7 @@ import Head from '../component/header/Head'
 import Footer from '../component/footer/Footer'
 import Mainone from '../component/aboutPages/main/mainone'
 import Years from '../component/aboutPages/years/Years'
+import Choose from '../component/aboutPages/choose/Choose'
 
 const About = () => {
   return (
@@ -10,6 +11,7 @@ const About = () => {
       <Head />
       <Mainone />
       <Years />
+      <Choose />
       <Footer />
     </>
   )
